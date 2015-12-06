58c5ea3a4cb99181cc10a22cb523c115d951a1be
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2015-12-06 12:28:47 +0100

    Solved 1-2

--- lib/advent/day_1.ex
+++ lib/advent/day_1.ex
@@ -1,16 +1,28 @@
 defmodule Advent.Day1 do
-  def run(input) do
+  def find_floor(input) do
     input
     |> String.to_char_list
-    |> process(0)
+    |> find_floor(0)
+  end
+
+  def basement_position(input) do
+    input
+    |> String.to_char_list
+    |> basement_position(1, 0)
   end
 
   def exercise_input do
     File.read!("data/input_day_1")
   end
 
-  defp process([], floor), do: floor
-  defp process([?( | rest], floor), do: process(rest, floor + 1)
-  defp process([?) | rest], floor), do: process(rest, floor - 1)
-  defp process([_ | rest], floor), do: process(rest, floor)
+  defp find_floor([], floor), do: floor
+  defp find_floor([?( | rest], floor), do: find_floor(rest, floor + 1)
+  defp find_floor([?) | rest], floor), do: find_floor(rest, floor - 1)
+  defp find_floor([_ | rest], floor), do: find_floor(rest, floor)
+
+  defp basement_position([?) | _rest], position, 0), do: position
+  defp basement_position([?( | rest], position, floor), do: basement_position(rest, position + 1, floor + 1)
+  defp basement_position([?) | rest], position, floor), do: basement_position(rest, position + 1, floor - 1)
+  defp basement_position([], _, _), do: nil
+
 end
--- test/day_1_test.exs
+++ test/day_1_test.exs
@@ -3,10 +3,16 @@ defmodule Advent.Day1Test do
   alias Advent.Day1
   doctest Advent
 
-  test "simple examples" do
-    assert Day1.run("(())") == 0
-    assert Day1.run("()()") == 0
-    assert Day1.run("(((") == 3
+  test "find_floor" do
+    assert Day1.find_floor("(())") == 0
+    assert Day1.find_floor("()()") == 0
+    assert Day1.find_floor("(((") == 3
+  end
+
+  test "basement_position" do
+    assert Day1.basement_position(")") == 1
+    assert Day1.basement_position("(()))") == 5
+    assert Day1.basement_position("(())") == nil
   end
 end
 
