8d96f28ff9ae876cd8af7baaee549605ee4214c5
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2015-12-06 21:33:38 +0100

    Solved 2-2

--- lib/advent/day_2.ex
+++ lib/advent/day_2.ex
@@ -1,8 +1,14 @@
 defmodule Advent.Day2 do
-  def calc_need(input) do
+  def wrapping_need(input) do
     input
     |> parse
-    |> calc(0)
+    |> calc_wrapping_need(0)
+  end
+
+  def ribbon_need(input) do
+    input
+    |> parse
+    |> calc_ribbon_need(0)
   end
 
   def input do
@@ -23,12 +29,21 @@ defmodule Advent.Day2 do
     |> Enum.map(&String.to_integer/1)
   end
 
-  defp calc([], total), do: total
-  defp calc([package | rest], total), do: calc(rest, total + calc_one(package))
+  defp calc_wrapping_need([], total), do: total
+  defp calc_wrapping_need([package | rest], total), do: calc_wrapping_need(rest, total + calc_one_wrapping(package))
 
-  defp calc_one(package) do
+  defp calc_one_wrapping(package) do
     package = Enum.sort(package)
     [a, b, c] = package
     2*a*b + 2*a*c + 2*b*c + a*b
+  end
+
+  defp calc_ribbon_need([], total), do: total
+  defp calc_ribbon_need([package | rest], total), do: calc_ribbon_need(rest, total + calc_one_ribbon(package))
+
+  defp calc_one_ribbon(package) do
+    package = Enum.sort(package)
+    [a, b, c] = package
+    2*a + 2*b + a*b*c
   end
 end
