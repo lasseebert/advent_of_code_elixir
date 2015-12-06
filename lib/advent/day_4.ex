29cdad5a714982f47a47d121d51b9add36ae06dc
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2015-12-06 22:59:47 +0100

    Solved 4-1 and 4-2

---
+++ lib/advent/day_4.ex
@@ -0,0 +1,21 @@
+defmodule Advent.Day4 do
+  def input do
+    "yzbqklnj"
+  end
+
+  def mine_advent_coin(secret, number \\ 1) do
+    case md5(secret <> Integer.to_string(number)) do
+      <<0 :: size(20), _ :: size(4)>> <> _ -> number
+      _ -> mine_advent_coin(secret, number + 1)
+    end
+  end
+
+  def mine_hard_advent_coin(secret, number \\ 1) do
+    case md5(secret <> Integer.to_string(number)) do
+      <<0 :: size(24)>> <> _ -> number
+      _ -> mine_hard_advent_coin(secret, number + 1)
+    end
+  end
+
+  defp md5(string), do: :crypto.hash(:md5, string)
+end
---
+++ test/day_4_test.exs
@@ -0,0 +1,10 @@
+defmodule Advent.Day4Test do
+  use ExUnit.Case
+  alias Advent.Day4
+  doctest Advent
+
+  test "mine_advent_coin" do
+    assert Day4.mine_advent_coin("abcdef") == 609043
+    assert Day4.mine_advent_coin("pqrstuv") == 1048970
+  end
+end
