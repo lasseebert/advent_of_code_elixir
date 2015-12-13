b8937a97fcc6f39efb04dfe12501b5d446a7b878
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2015-12-13 23:23:15 +0100

    solved 8-2

--- lib/advent/day_8.ex
+++ lib/advent/day_8.ex
@@ -20,4 +20,25 @@ defmodule Advent.Day8 do
     codes
     |> Enum.reduce(0, fn code, acc -> overhead(code) + acc end)
   end
+
+  def overhead_2(code) do
+    ~R/(?:\\")|(?:\\\\)|(?:\\x[a-f0-9]{2})/i
+    |> Regex.scan(code)
+    |> Enum.reduce(0, fn
+      [~S(\\)], acc ->
+        2 + acc
+      [~S(\")], acc ->
+        2 + acc
+      [_ascii], acc ->
+        1 + acc
+      [], acc ->
+        acc
+    end)
+    |> :erlang.+(4)
+  end
+
+  def overheads_2(codes) do
+    codes
+    |> Enum.reduce(0, fn code, acc -> overhead_2(code) + acc end)
+  end
 end
--- test/day_8_test.exs
+++ test/day_8_test.exs
@@ -28,4 +28,31 @@ defmodule Advent.Day8Test do
 
     assert overheads(input) == 12
   end
+
+  test "Overhead 2: No escaping" do
+    assert overhead_2(~S("")) == 4
+  end
+
+  test "Overhead 2: Double backslash" do
+    assert overhead_2(~S("abc\\abc")) == 6
+  end
+
+  test "Overhead 2: Escaped quote" do
+    assert overhead_2(~S("abc\"abc")) == 6
+  end
+
+  test "Overhead 2: Escaped ascii" do
+    assert overhead_2(~S("abc\xe0abc")) == 5
+  end
+
+  test "Overheads 2: Sample input" do
+    input = [
+      ~S(""),
+      ~S("abc"),
+      ~S("aaa\"aaa"),
+      ~S("\x27")
+    ]
+
+    assert overheads_2(input) == 19
+  end
 end
