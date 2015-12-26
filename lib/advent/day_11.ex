5c3314042d18d4a1f02772bf316d9c54b2d2cf9f
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2015-12-27 00:20:30 +0100

    Performance optimization on day11

--- lib/advent/day_11.ex
+++ lib/advent/day_11.ex
@@ -4,6 +4,26 @@ defmodule Advent.Day11 do
   end
 
   def next_password(old_password) do
+    old_password
+    |> normalize("")
+    |> do_next_password
+  end
+
+  defp normalize(<<?i, rest::binary>>, prefix) do
+    prefix <> <<?j>> <> String.duplicate("a", String.length(rest))
+  end
+  defp normalize(<<?l, rest::binary>>, prefix) do
+    prefix <> <<?m>> <> String.duplicate("a", String.length(rest))
+  end
+  defp normalize(<<?o, rest::binary>>, prefix) do
+    prefix <> <<?p>> <> String.duplicate("a", String.length(rest))
+  end
+  defp normalize(<<a, rest::binary>>, prefix) do
+    normalize(rest, prefix <> <<a>>)
+  end
+  defp normalize("", prefix), do: prefix
+
+  defp do_next_password(old_password) do
     next = increment(old_password)
     if valid?(next) do
       next
@@ -29,8 +49,13 @@ defmodule Advent.Day11 do
     increment_digit(rest, [?a | acc])
   end
   defp increment_digit([digit | rest], acc) do
-    ([digit + 1 | rest] |> Enum.reverse) ++ acc
+    ([next_digit(digit) | rest] |> Enum.reverse) ++ acc
   end
+
+  defp next_digit(?h), do: ?j
+  defp next_digit(?k), do: ?m
+  defp next_digit(?n), do: ?p
+  defp next_digit(digit), do: digit + 1
 
   defp valid?(password) do
     [
