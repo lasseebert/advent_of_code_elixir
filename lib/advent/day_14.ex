c6432e9e558e8f3939ff044991418386722711ce
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2015-12-28 00:08:55 +0100

    Solved 14-2

--- lib/advent/day_14.ex
+++ lib/advent/day_14.ex
@@ -1,4 +1,22 @@
 defmodule Advent.Day14 do
+  defmodule Reindeer do
+    defstruct(
+      speed: 0,
+      fly_seconds: 1,
+      rest_seconds: 1,
+      score: 0,
+      distance: 0,
+      time: 0
+    )
+
+    def travel(%Reindeer{time: time, distance: distance, speed: speed, fly_seconds: fly_seconds, rest_seconds: rest_seconds} = reindeer) do
+      new_distance = if time < fly_seconds, do: distance + speed, else: distance
+      new_time = if time + 1 < fly_seconds + rest_seconds, do: time + 1, else: 0
+
+      %{reindeer | distance: new_distance, time: new_time}
+    end
+  end
+
   def default_input do
     File.read!("data/input_day_14")
   end
@@ -7,6 +25,14 @@ defmodule Advent.Day14 do
     input
     |> parse
     |> Enum.map(&travel(&1, seconds))
+    |> Enum.max
+  end
+
+  def most_points(input, seconds) do
+    input
+    |> parse
+    |> travel_all_and_collect_score(seconds)
+    |> Enum.map(fn reindeer -> reindeer.score end)
     |> Enum.max
   end
 
@@ -30,17 +56,32 @@ defmodule Advent.Day14 do
     } = ~r/^.* can fly (?<speed>\d+) km\/s for (?<fly_seconds>\d+) seconds, but then must rest for (?<rest_seconds>\d+) seconds\.$/
         |> Regex.named_captures(line)
 
-    %{
+    %Reindeer{
       speed: String.to_integer(speed),
       fly_seconds: String.to_integer(fly_seconds),
       rest_seconds: String.to_integer(rest_seconds)
     }
   end
 
-  defp travel(%{speed: speed, fly_seconds: fly_seconds, rest_seconds: rest_seconds}, seconds) do
+  defp travel(%Reindeer{speed: speed, fly_seconds: fly_seconds, rest_seconds: rest_seconds}, seconds) do
     period = fly_seconds + rest_seconds
     periods = div(seconds, period)
     rest = rem(seconds, period)
     speed * (periods * fly_seconds + Enum.min([rest, fly_seconds]))
+  end
+
+  defp travel_all_and_collect_score(reindeers, 0), do: reindeers
+  defp travel_all_and_collect_score(reindeers, seconds) do
+    reindeers = Enum.map(reindeers, &Reindeer.travel/1)
+    best_distance = reindeers
+                    |> Enum.map(fn reindeer -> reindeer.distance end)
+                    |> Enum.max
+    reindeers = Enum.map(reindeers, fn
+      %Reindeer{distance: ^best_distance, score: score} = reindeer ->
+        %{reindeer | score: score + 1}
+      reindeer ->
+        reindeer
+    end)
+    travel_all_and_collect_score(reindeers, seconds - 1)
   end
 end
--- test/day_14_test.exs
+++ test/day_14_test.exs
@@ -2,14 +2,18 @@ defmodule Advent.Day14Test do
   use ExUnit.Case
   import Advent.Day14
 
-  test "longest_distance" do
-    input = """
+  def example_input do
+    """
       Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
       Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.
     """
+  end
 
-    assert longest_distance(input, 1000) == 1120
+  test "longest_distance" do
+    assert longest_distance(example_input, 1000) == 1120
+  end
+
+  test "most_points" do
+    assert most_points(example_input, 1000) == 689
   end
 end
-
-
